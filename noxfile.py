5b210bc98b12d23c9402d723566f1cd0d5521046
Otto Fowler <ottobackwards@gmail.com>
2022-08-19 14:52:12 -0400

    flak8

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,7 @@
 from nox_poetry import session
 
+locations = "simple_syslog", "tests", "noxfile.py"
+
 
 @session(python=["3.8", "3.9"])
 def tests(session):
@@ -11,3 +13,10 @@ def tests(session):
     session.install("cleo", ".")
     session.run("poetry", "install", external=True)
     session.run("pytest", *args, external=True)
+
+
+@session(python=["3.8", "3.9"])
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
