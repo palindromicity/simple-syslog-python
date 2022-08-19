6a2bb06bb9da8cd4524ffa8f8d04576e075f69b8
Otto Fowler <ottobackwards@gmail.com>
2022-08-19 15:54:26 -0400

    pre-commit, black, flake

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,9 @@
+from nox import options
 from nox_poetry import session
 
+# we by default only want to lint and test
+# we don't want to modify with black
+options.sessions = "lint", "tests"
 locations = "simple_syslog", "tests", "noxfile.py"
 
 
@@ -18,5 +22,12 @@ def tests(session):
 @session(python=["3.8", "3.9"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8")
+    session.install("black", "flake8", "flake8-black", "flake8-bugbear", "flake8-isort")
     session.run("flake8", *args)
+
+
+@session(python=["3.8", "3.9"])
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
