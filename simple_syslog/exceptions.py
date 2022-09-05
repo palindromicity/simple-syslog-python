f95cb2459287d62c54c81496a2d85682f046a337
Otto Fowler <ottobackwards@gmail.com>
2022-09-04 23:48:13 -0400

    error strategy (#3)

--- simple_syslog/exceptions.py
+++ simple_syslog/exceptions.py
@@ -11,9 +11,70 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from antlr4 import Parser, RecognitionException
+from antlr4.error.ErrorListener import ErrorListener
+from antlr4.error.ErrorStrategy import DefaultErrorStrategy
 
 
 class DeviationError(Exception):
     """Custom Error raised for Data missing without an AllowedDeviation."""
 
     pass
+
+
+class ParseError(Exception):
+    """Custom Error raised for parse errors."""
+
+    pass
+
+
+class SimpleErrorStrategy(DefaultErrorStrategy):
+    """DefaultErrorStrategy raising a ParseError."""
+
+    def reportError(self, recognizer: Parser, e: RecognitionException) -> None:
+        """Reports parsing errors.
+
+        Args:
+            recognizer: The Parser
+            e: The RecognitionException
+
+        Raises:
+            ParseError: With the error
+
+        """
+        raise ParseError("Parse Error " + e.message, e)
+
+
+# noqa
+
+
+class SimpleErrorListener(ErrorListener):
+    """Simple ErrorListener implementation."""
+
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        """Handle Syntax Errors."""
+        raise ParseError(f"Syntax error @ {line}:{column}  {msg}", e)
+
+    def reportAmbiguity(
+        self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs
+    ):
+        """Handle Ambiguity Reports."""
+        super().reportAmbiguity(
+            recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs
+        )
+
+    def reportAttemptingFullContext(
+        self, recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs
+    ):
+        """Handle Attempting Full Context Reports."""
+        super().reportAttemptingFullContext(
+            recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs
+        )
+
+    def reportContextSensitivity(
+        self, recognizer, dfa, startIndex, stopIndex, prediction, configs
+    ):
+        """Handle Context Sensitivity Reports."""
+        super().reportContextSensitivity(
+            recognizer, dfa, startIndex, stopIndex, prediction, configs
+        )
